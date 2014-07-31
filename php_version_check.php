1b603bf0bdac737d562e580aec1ed9ba48fc9fe4
Ronald01990 <rlwedelschaap@gmail.com>
2014-07-31 14:10:18 +0200

    Create php_version_check.php
    Include this file in your index.php file in the root directory.

---
+++ php_version_check.php
@@ -0,0 +1,19 @@
+<?php
+	require_once(realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'phpFunctionsCrawler.class.php');
+	require_once(realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'phpVersionCheck.class.php');
+
+	if (!empty($_GET['action']) && $_GET['action'] == 'crawl_functions') {
+		$php_crawler = new phpFunctionsCrawler();
+		$php_crawler->scrapeFunctionIndex();
+		$php_crawler->scrapeFunctions();
+
+		$php_crawler->saveToXMLFile('functions/functions.xml');
+	}
+
+	if (!empty($_GET['action']) && $_GET['action'] == 'check_version') {
+		$php_version_checker = new phpVersionCheck('functions/functions.xml');
+		$php_version_checker->checkFiles();
+		$php_min_version = $php_version_checker->getResults();
+
+		echo $php_min_version['required_version'];
+	}
